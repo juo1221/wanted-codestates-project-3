e1e614aa93c56e2b90db958d212033e3452504cb
juo <hwgiwh271@gmail.com>
2022-03-25 03:20:08 +0900

    [Refactor] #7 App 구현
    -

--- src/app.js
+++ src/app.js
@@ -1,5 +1,36 @@
 import './app.scss';
+import HomeController from '@Components/controller/HomeController';
+import { append } from '@Utils/util';
+import { el } from '@Utils/util';
+import { err } from '@Utils/util';
+import { sel } from '@Utils/util';
 
-// import DomRenderer from '@Components/render/DomRenderer';
-// const renderer = new DomRenderer(['#container-avaliable', '#container-selected']);
-// renderer.data = renderer.render();
+const App = class extends Map {
+  #parent;
+  constructor(_parent = err(`no parent`)) {
+    super();
+    this.#parent = _parent;
+  }
+  set() {
+    err();
+  }
+  has() {
+    err();
+  }
+  delete() {
+    err();
+  }
+  add(k = err(`no path`), controllerF = err(`no controller`)) {
+    if (!super.has(k)) super.set(k, controllerF);
+  }
+  route(path, ...arg) {
+    if (!super.has(path)) err(`no path in App : ${path}`);
+    const controller = super.get(path)();
+    append(el(sel(this.#parent), 'innerHTML', ''), controller['base'](...arg));
+  }
+};
+const app = new App('#main');
+app.add('home', (_) => new HomeController(true));
+app.route('home');
+
+export default app;
